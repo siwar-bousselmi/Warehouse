e66a692a569bcc73bd7ee58a0fa1daed8a9c12d1
siwar-mae <siwbouss@gmail.com>
2024-08-31 13:14:40 +0000

    dev login

---
+++ src/app/features/warehouses/warehouses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouses',
+  standalone: true,
+  imports: [],
+  templateUrl: './warehouses.component.html',
+  styleUrl: './warehouses.component.scss',
+})
+export class WarehousesComponent {
+
+}
